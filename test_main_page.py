0409e2a6f4cd7d8a544300665cf32067f4a12896
hath108 <sbksbk@ya.ru>
2019-12-20 10:53:45 +0300

    добавили переходы между страниц

--- test_main_page.py
+++ test_main_page.py
@@ -12,3 +12,8 @@ def test_guest_can_go_to_login_page(browser):
     page.open()  # class BasePage()
     page.should_be_login_link()  # class MainPage(BasePage)
     page.go_to_login_page()  # class MainPage(BasePage)
+    #
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_url()  # class LoginPage(BasePage)
+    login_page.should_be_login_form()  # class LoginPage(BasePage)
+    login_page.should_be_register_form()  # class LoginPage(BasePage)
